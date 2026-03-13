f5a195400d89201042a09847fbd81caf776539b9
Sokol111 <igorsokol111@gmail.com>
2026-03-13 21:10:04 +0100

    u

--- gen/typescript/api.schemas.ts
+++ gen/typescript/api.schemas.ts
@@ -3,7 +3,7 @@
  * Do not edit manually.
  * Product query API
  * API for querying products
- * OpenAPI spec version: 1.1.0
+ * OpenAPI spec version: 1.1.1
  */
 export interface ProductListResponse {
   items: ProductResponse[];
